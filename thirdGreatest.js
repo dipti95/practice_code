cd2cd7d0734762f78d47370273e478b9fb9de4f7
dipti <dipti.yadav@outlook.com>
2021-06-11 13:13:55 -0400

    solved

--- thirdGreatest.js
+++ thirdGreatest.js
@@ -1,3 +1,27 @@
-function thirdGreatest(array){
-   // use sort in this problem 
-}
+function ThirdGreatest(strArr) {
+   let top3 = ['', '', ''];
+   for (let i = 0; i < strArr.length; i++)
+     if (strArr[i].length > top3[0].length)
+       if (strArr[i].length > top3[1].length)
+         if (strArr[i].length > top3[2].length) top3 = [top3[1], top3[2], strArr[i]];
+         else top3 = [top3[1], strArr[i], top3[2]];
+       else top3 = [strArr[i], top3[2], top3[3]];
+   return top3[0];
+ }
+
+ // ----------------
+ function ThirdGreatest(strArr) {
+   let currentMax;
+   for (let i = 0; i < 3; i++) {
+     let maxIdx = 0;
+     for (let j = 0; j < strArr.length; j++)
+       if (strArr[j].length > strArr[maxIdx].length) maxIdx = j;
+     currentMax = strArr.splice(maxIdx, 1)[0];
+   }
+   return currentMax;
+ }
+ //------------------------
+ function ThirdGreatest(strArr) {
+   function sortByLength(a, b) { return b.length - a.length; }
+   return strArr.sort(sortByLength)[2];
+ }
