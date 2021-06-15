2609c6ea58d9a22a269a846aab6c27f1cdd9013e
dipti <dipti.yadav@outlook.com>
2021-06-15 14:43:03 -0400

    leetcode

---
+++ countAndSay.js
@@ -0,0 +1,25 @@
+//-----------------------
+//STRING
+
+function countAndSay(n){
+    if(n === 1) return '1';
+    if(n === 2) return '11';
+     let str = '11'
+     for(let i = 3; i <= n; i++){
+         str += '$';
+         let len = str.length
+         let count = 1;
+         let newStr = ''
+         let arr = str.split('');
+         for(let j = 1; j < len; j++){
+             if(arr[j] !== arr[j-1]){
+                 newStr += count ;
+                 newStr += arr[j-1];
+                 count =1
+             }else count ++
+         }
+         str = newStr
+     }
+     return str;
+}
+console.log(countAndSay(7))
