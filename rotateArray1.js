14c409740d6c6280b3e911b30662cab084dfa0bd
dipti <dipti.yadav@outlook.com>
2021-10-16 11:39:48 -0400

    revision

--- rotateArray1.js
+++ rotateArray1.js
@@ -1,12 +1,18 @@
 function rotateArray(originalArray, rotateNum) {
-    let result = [];
-    if (rotateNum > 0) {
-      for (let i = originalArray.length - rotateNum; i < originalArray.length; i++) result.push(originalArray[i]);
-      for (let i = 0; i < originalArray.length - rotateNum; i++) result.push(originalArray[i]);
-    } else {
-      for (let i = -rotateNum; i < originalArray.length; i++) result.push(originalArray[i]);
-      for (let i = 0; i < -rotateNum; i++) result.push(originalArray[i]);
-    }
-    return result;
+  let result = [];
+  if (rotateNum > 0) {
+    for (
+      let i = originalArray.length - rotateNum;
+      i < originalArray.length;
+      i++
+    )
+      result.push(originalArray[i]);
+    for (let i = 0; i < originalArray.length - rotateNum; i++)
+      result.push(originalArray[i]);
+  } else {
+    for (let i = -rotateNum; i < originalArray.length; i++)
+      result.push(originalArray[i]);
+    for (let i = 0; i < -rotateNum; i++) result.push(originalArray[i]);
   }
-  
+  return result;
+}
