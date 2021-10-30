a4aceb7233b415a79ef5a99cb660973f7cd8217d
dipti <dipti.yadav@outlook.com>
2021-10-29 20:41:47 -0400

    search in Rotated sorted Array

---
+++ leetCodeProblem/ArrayProblem/searchInRotatedSortedArray.js
@@ -0,0 +1,23 @@
+// 0(log n) time complexity;
+// By using binary search;
+
+function searchTarget(nums, target) {
+  let left = 0;
+  let right = nums.length;
+  while (left < right) {
+    let mid = (left + right) / 2;
+    if (nums[mid] === target) return mid;
+    if (nums[left] <= target) {
+      if (nums[left] <= nums[mid] && target <= nums[right]) right = mid - 1;
+      else left = mid + 1;
+    } else {
+      if (nums[mid] <= target && target <= nums[right]) right = mid + 1;
+      else left = mid - 1;
+    }
+  }
+  return -1;
+}
+
+// let nums = [4, 5, 6, 7, 0, 1, 2];
+// let target = 3;
+// console.log(searchTarget(nums, target));
