57b2ec1f6e725de7ca59c1c6f6bd5cb9eb9bc4d1
dipti <dipti.yadav@outlook.com>
2021-10-30 20:37:02 -0400

    validParenthesis

---
+++ leetCodeProblem/StringProblem/validParentheses.js
@@ -0,0 +1,21 @@
+function validParen(str) {
+  let openParen = "({[";
+  let closeParen = ")}]";
+  let hash = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
+  let stack = [];
+
+  for (let ele of str) {
+    if (openParen.includes(ele)) stack.push(ele);
+    else {
+      if (stack.length === 0) return false;
+      else if (hash[ele] === stack[stack.length - 1]) stack.pop();
+      else return false;
+    }
+  }
+  if (stack.length === 0) return true;
+  else return false;
+}
