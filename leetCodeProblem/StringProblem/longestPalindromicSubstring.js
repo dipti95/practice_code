55586df0a6fd1f10fa13fbac679deac9dd34f067
dipti <dipti.yadav@outlook.com>
2021-10-31 20:26:01 -0400

    Longest Palindromic Substring

--- leetCodeProblem/StringProblem/longestPalindromicSubstring.js
+++ leetCodeProblem/StringProblem/longestPalindromicSubstring.js
@@ -22,3 +22,26 @@ function isPalindrome(str) {
   }
   return true;
 }
+
+// better way to solve this
+
+function longestPalindrome(s) {
+  let current = [0, 1];
+  for (let i = 1; i < s.length; i++) {
+    let odd = palindrome(s, i - 1, i + 1);
+    let even = palindrome(s, i - 1, i);
+    let longest = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
+    current =
+      current[1] - current[0] > longest[1] - longest[0] ? current : longest;
+  }
+  return s.slice(current[0], current[1]);
+}
+
+function palindrome(string, leftIdx, rightIdx) {
+  while (leftIdx >= 0 && rightIdx < string.length) {
+    if (string[leftIdx] !== string[rightIdx]) break;
+    leftIdx--;
+    rightIdx++;
+  }
+  return [leftIdx + 1, rightIdx];
+}
