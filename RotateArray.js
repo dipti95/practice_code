6bf8923d2dafd69f5b65d587676073ac43cfe28d
dipti <dipti.yadav@outlook.com>
2021-11-05 19:47:03 -0400

    anagram

--- RotateArray.js
+++ RotateArray.js
@@ -1,33 +1,29 @@
 function rotate(nums, k) {
-    if (nums === null || nums.length === 0) {
-        return;
-    }
-    k = k % nums.length;
-    if (k === 0) {
-        return;
-    }
-    nums.reverse();
-    reverse(nums, 0, k-1);
-    reverse(nums, k, nums.length - 1);
-};
+  if (nums === null || nums.length === 0) {
+    return;
+  }
+  k = k % nums.length;
+  if (k === 0) {
+    return;
+  }
+  nums.reverse();
+  reverse(nums, 0, k - 1);
+  reverse(nums, k, nums.length - 1);
+}
 
 const reverse = (arr, start, end) => {
-    while (start < end) {
-        let temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-        start++, end--;
-    }
-}
+  while (start < end) {
+    let temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++, end--;
+  }
+};
 // ------------------------
 
 function rotate(nums, k) {
-    let front = nums.slice(-k);
-    let end = nums.slice(0, -k);
-    console.log(front.concat(end))
-    return front.concat(end);
-  }
-
-
-  
-  
+  let front = nums.slice(-k);
+  let end = nums.slice(0, -k);
+  console.log(front.concat(end));
+  return front.concat(end);
+}
--- validAnagram.js
+++ validAnagram.js
@@ -1,13 +1,29 @@
-function validAnagram(s,t){
-    if(s.length !== t.length) return false;
-    let hash = {}
-    for(let ele of s){
-        if(ele in hash) hash[ele] += 1;
-        else hash[ele] =1
-    }
-    for(let ele of t){
-        if(hash[ele]) hash[ele]--;
-        else return false
-    }
-    return true;
+// Two words are given and we have to check that they are anagram or not and return the boolean value;
+
+function validAnagram(s, t) {
+  if (s.length !== t.length) return false;
+  let obj = {};
+
+  for (let ele of s) {
+    if (ele in obj) obj[ele] += 1;
+    else obj[ele] = 1;
+  }
+
+  for (let ele of t) {
+    if (hash[ele]) hash[ele]--;
+    else return false;
+  }
+  return true;
+}
+
+// Important
+function groupAnagram(array) {
+  let obj = {};
+  for (let ele of array) {
+    let item = ele.split("").sort().join("");
+    if (item in obj) obj[item].push(ele);
+    else obj[item].push([ele]);
+  }
+  let value = Object.values(obj);
+  return value;
 }
