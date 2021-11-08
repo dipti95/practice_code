230ca2e64ed850d3eafbef7b6d24ca2cdc8b8523
dipti <dipti.yadav@outlook.com>
2021-11-08 07:09:36 -0500

    Unique Binary Search Tree

--- leetCodeProblem/Tree/uniqueBinarySearchTrees.js
+++ leetCodeProblem/Tree/uniqueBinarySearchTrees.js
@@ -9,3 +9,14 @@ function numTrees(n) {
   }
   return arr[n];
 }
+
+// recursive solution ;
+
+function numTrees(n) {
+  return factorial(2 * n) / (factorial(n + 1) * factorial(n));
+}
+
+function factorial(num) {
+  if (num <= 0) return 1;
+  else return num * factorial(num - 1);
+}
