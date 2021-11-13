4986b21be7b074ff27c205f8bb28fe5ebe62fa4e
dipti <dipti.yadav@outlook.com>
2021-11-13 12:16:46 -0500

    challenge

--- basicsOfJavascript/node.js
+++ basicsOfJavascript/node.js
@@ -28,9 +28,9 @@ const students = [
 //********** Challenge 1 **********************
 ////////Create an array of all the id values.///////
 //example: [1, 2, 3, 4]
-function getId(students){
+function getId(x){
    let arrId = [];
-   for(let ele of students){
+   for(let ele of x){
        arrId.push(ele.id);
    }
    
@@ -42,49 +42,67 @@ console.log(getId(students))
 // //********** Challenge 2 **********************
 // ////////create an array of students 21 and over./////
 // //example: [{id: 1,first_name: "John",last_name: "Smith",age: 21}, etc.....]
+​function getStudent(x){
+    let arr = []
+    for(let ele of x){
+        if(ele.age >= 21) arr.push(ele);
+    }
+    return arr
+}
+console.log(getStudent(students))
+
 ​
 ​
 ​
 // // //********** Challenge 3 **********************
 // // ////////get the first object from the array of students who is older than 15 but younger than 25/////
+function student(x){
+    let arr = 0;
+    for(let ele of x){
+        if(ele.age > 15 && ele.age <25) arr.push(ele)
+    }
+    return arr
+}
+console.log(student(students))
 ​
 ​
 ​
 ​
 //********** Challenge 4 **********************
 //un-comment the following lines (the object and the console.log).  This will cause an errors. Fix the errors so it will work correctly.
-// const myObj = {
-//     name: 'Chris',
-//     age: 16,
-//     location: "Florida",
-//     is_developer: true,
-//     grades: {
-//     	math: "A"
-//     	english: "A",
-//     	science: "B",
-//     }
+const myObj = {
+    name: 'Chris',
+    age: 16,
+    location: "Florida",
+    is_developer: true,
+    grades: {
+    	math: "A",
+    	english: "A",
+    	science: "B",
+    }
 ​
-// };
-// console.log(myObj.grad.math);
-// console.log(myobj.grades.math);
+};
+console.log(myObj.grades.math);
+console.log(myObj.grades.math);
 ​
 //********** Challenge 5 **********************
 //// If this was a script, Which one will work, how do you fix the errors.  Explain the behavior ////
 //// uncommennt the function and function calls below ////
 ​
 ​
-// funcOne();
-// funcTwo();
-// funcThree();
+
 ​
-// function funcOne () {
-// 	console.log('funcOne called.');
-// }
-// var funcTwo = function () {
-//     console.log('funcTwo called.');
-// }
-// const funcThree  = function () {
-//     console.log('funcThree called.');
-// }
+function funcOne () {
+	console.log('funcOne called.');
+}
+var funcTwo = function () {
+    console.log('funcTwo called.');
+}
+const funcThree  = function () {
+    console.log('funcThree called.');
+}
+​funcOne();
+funcTwo();
+funcThree();
 ​
 ​
