ad3f9da3336ff6efd32bf32c05f3ab4470366cb6
dipti <dipti.yadav@outlook.com>
2021-11-17 07:13:01 -0500

    Unique path

--- leetCodeProblem/uniquePath.js
+++ leetCodeProblem/uniquePath.js
@@ -15,3 +15,16 @@ const uniquePaths = (m, n) => {
 
   return dummyArray[1][1];
 };
+
+// we can do recursive solution also;
+const uniquePaths = (m, n) => {
+  return getCount(m, n, 1, 1);
+};
+function getCount(m, n, row, col) {
+  if (row === m && col === n) return 1;
+  if (row > m && col > n) return 0;
+
+  let pathRight = getCount(m, n, row, col + 1);
+  let pathDown = getCount(m, n, row + 1, col);
+  return pathDown + pathRight;
+}
