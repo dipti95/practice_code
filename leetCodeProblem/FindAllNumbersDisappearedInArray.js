17734b37a6cff5d75b6d9d0de28212c15ed7f039
dipti <dipti.yadav@outlook.com>
2021-11-18 13:40:51 -0500

    Find Disappered Number

--- leetCodeProblem/FindAllNumbersDisappearedInArray.js
+++ leetCodeProblem/FindAllNumbersDisappearedInArray.js
@@ -1,3 +1,4 @@
+// Run time is more in this solution
 function findDisappearedNumbers(nums) {
   let arr = [];
 
@@ -14,4 +15,18 @@ function findDisappearedNumbers(nums) {
   }
 
   return result;
+}
+
+// Another way - Run time is less then previous solution;
+function findDisappearedNumbers(nums) {
+  let res = [];
+  for (let i = 0; i < nums.length; i++) {
+    let num = Math.abs(nums[i]);
+    let idx = num - 1;
+    nums[idx] = Math.abs(nums[idx]) * -1;
+  }
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) res.push(i + 1);
+  }
+  return res;
 }
