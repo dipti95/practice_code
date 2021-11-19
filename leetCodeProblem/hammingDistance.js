76707fec1a95f897d00fe88ea05850c0cedb1a52
dipti <dipti.yadav@outlook.com>
2021-11-19 11:49:55 -0500

    Hamming Distance

---
+++ leetCodeProblem/hammingDistance.js
@@ -0,0 +1,12 @@
+function hammingDistance(x, y) {
+  let value = x ^ y;
+  let counter = 0;
+
+  while (value != 0) {
+    if (value & 1) ++counter;
+
+    value = value >> 1;
+  }
+
+  return counter;
+}
