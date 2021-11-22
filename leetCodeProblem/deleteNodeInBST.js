0f36d778ab3996b7780fda8b1f15097c073028a5
dipti <dipti.yadav@outlook.com>
2021-11-22 07:24:42 -0500

    Delete Node in BST

--- leetCodeProblem/deleteNodeInBST.js
+++ leetCodeProblem/deleteNodeInBST.js
@@ -1,7 +1,9 @@
 function deleteNode(root, key) {
   if (!root) return null;
   if (root.val === key) {
+    // to delete a leaf node (no children) we can just return null
     if (!root.left) return root.left;
+    // to delete a leaf node (no children) we can just return null
     if (!root.right) return root.right;
     let currentVal = root.right;
     while (currentVal.left) currentVal = currentVal.left;
