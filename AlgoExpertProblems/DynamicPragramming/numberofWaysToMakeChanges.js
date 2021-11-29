46064c073461fea17a67e7a51aceb7c8de88fdd9
dipti <dipti.yadav@outlook.com>
2021-11-29 08:22:48 -0500

    Number of Ways To Make Changes

--- AlgoExpertProblems/DynamicPragramming/numberofWaysToMakeChanges.js
+++ AlgoExpertProblems/DynamicPragramming/numberofWaysToMakeChanges.js
@@ -1,3 +1,7 @@
+/* let say if n = 6 denoms= [1,5]  
+so, number of ways to get 6 is 2 => 1*1 +5*1 and 6*1
+*/
+
 function numberOfWays(n, denoms) {
   let dummyArr = new Array(n + 1).fill(0);
   dummyArr[0] = 1;
