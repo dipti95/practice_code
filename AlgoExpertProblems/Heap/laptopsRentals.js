88c5b87558bc934f76de2f1d07c40544dc0b9c26
dipti <dipti.yadav@outlook.com>
2021-11-30 11:08:39 -0500

    Laptop Rentals

---
+++ AlgoExpertProblems/Heap/laptopsRentals.js
@@ -0,0 +1,9 @@
+/** 
+ Given => time interval of individual student [start, end]
+ 0<= start< end  . however start& end don't represent the actual time 
+ so, they may be greter than 24;
+ No, student can use laptop at the same time , but immediately after 
+ a studentis done using laptop,another student can use that same laptop
+ Minimun number of laptops that schoolneed to rent that 
+ all the students will always accessto the laptop when they need 
+ */
