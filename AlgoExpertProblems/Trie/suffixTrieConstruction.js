9f75df76b6874d7f07fbc81a9558ce2a786c50dd
dipti <dipti.yadav@outlook.com>
2021-12-01 15:19:04 -0500

    Suffix Trie Construction

---
+++ AlgoExpertProblems/Trie/suffixTrieConstruction.js
@@ -0,0 +1,22 @@
+class suffixTrie {
+  constructor(string) {
+    this.root = {};
+    this.endSymbol = "*";
+    this.populateSuffixTrieFrom(string);
+  }
+
+  populateSuffixTrieFrom(string) {
+    for (let i = 0; i < string.length; i++) {
+      insertString(string.slice(i));
+    }
+  }
+
+  insertString(string) {
+    let obj = this.root;
+    for (let ele of string) {
+      if (!(ele in obj)) obj[ele] = {};
+      obj = obj[ele];
+    }
+    obj[this.endSymbol] = true;
+  }
+}
