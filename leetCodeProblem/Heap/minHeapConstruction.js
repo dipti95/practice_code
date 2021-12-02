06a3fe2e815deedc492b7c173d3620f3e2e15de2
dipti <dipti.yadav@outlook.com>
2021-12-02 14:16:17 -0500

    Min Heap

--- leetCodeProblem/Heap/minHeapConstruction.js
+++ leetCodeProblem/Heap/minHeapConstruction.js
@@ -1,89 +1,89 @@
-// class MinHeap {
-//   constructor(array) {
-//     this.heap = this.buildHeap(array);
-//   }
+class MinHeap {
+  constructor(array) {
+    this.heap = this.buildHeap(array);
+  }
 
-//   buildHeap(array) {
-//     // formula to find parent node is
-//     // parentNode = Math.floor((index of given child node -1)/2)
-//     let FirstParentNodeIdx = Math.floor((array.length - 2) / 2);
-//     // Hear we are finding the parent node of last children node
-//     for (let i = FirstParentNodeIdx; i >= 0; i--) {
-//       this.siftDown(i, array.length - 1, array);
-//     }
-//     return array;
-//   }
-//   siftDown(currentIdx, endIdx, heap) {
-//     let firstChidIdx = 2 * currentIdx + 1;
+  buildHeap(array) {
+    // formula to find parent node is
+    // parentNode = Math.floor((index of given child node -1)/2)
+    let FirstParentNodeIdx = Math.floor((array.length - 2) / 2);
+    // Hear we are finding the parent node of last children node
+    for (let i = FirstParentNodeIdx; i >= 0; i--) {
+      this.siftDown(i, array.length - 1, array);
+    }
+    return array;
+  }
+  siftDown(currentIdx, endIdx, heap) {
+    let firstChidIdx = 2 * currentIdx + 1;
 
-//     while (firstChidIdx <= endIdx) {
-//       let secondChildIdx =
-//         2 * currentIdx + 2 <= endIdx ? 2 * currentIdx + 2 : -1;
-//       let swapIdx = 0;
-//       if (currentIdx != -1 && heap[secondChildIdx] < heap[firstChidIdx]) {
-//         swapIdx = secondChildIdx;
-//       } else swapIdx = firstChidIdx;
+    while (firstChidIdx <= endIdx) {
+      let secondChildIdx =
+        2 * currentIdx + 2 <= endIdx ? 2 * currentIdx + 2 : -1;
+      let swapIdx = 0;
+      if (currentIdx != -1 && heap[secondChildIdx] < heap[firstChidIdx]) {
+        swapIdx = secondChildIdx;
+      } else swapIdx = firstChidIdx;
 
-//       if (heap[swapIdx] < heap[currentIdx]) {
-//         swap(currentIdx, swapIdx, heap);
-//         currentIdx = swapIdx;
-//         firstChidIdx = currentIdx * 2 + 1;
-//       } else return;
-//     }
-//   }
+      if (heap[swapIdx] < heap[currentIdx]) {
+        swap(currentIdx, swapIdx, heap);
+        currentIdx = swapIdx;
+        firstChidIdx = currentIdx * 2 + 1;
+      } else return;
+    }
+  }
 
-//   siftUp(currentIdx, heap) {
-//     let parentIdx = Math.floor((currentIdx - 1) / 2);
-//     while (currentIdx > 0 && heap[currentIdx] < heap[parentIdx]) {
-//       swap(currentIdx, parentIdx, heap);
-//       currentIdx = parentIdx;
-//       parentIdx = Math.floor((currentIdx - 1) / 2);
-//     }
-//   }
+  siftUp(currentIdx, heap) {
+    let parentIdx = Math.floor((currentIdx - 1) / 2);
+    while (currentIdx > 0 && heap[currentIdx] < heap[parentIdx]) {
+      swap(currentIdx, parentIdx, heap);
+      currentIdx = parentIdx;
+      parentIdx = Math.floor((currentIdx - 1) / 2);
+    }
+  }
 
-//   peek() {
-//     return this.heap[0];
-//   }
+  peek() {
+    return this.heap[0];
+  }
 
-//   remove() {
-//     // removing root node ;
-//     swap(0, this.heap.length - 1, this.heap);
-//     let removeVal = this.heap.pop();
-//     this.siftDown(0, this.heap.length - 1, this.heap);
-//     return removeVal;
-//   }
+  remove() {
+    // removing root node ;
+    swap(0, this.heap.length - 1, this.heap);
+    let removeVal = this.heap.pop();
+    this.siftDown(0, this.heap.length - 1, this.heap);
+    return removeVal;
+  }
 
-//   insert(value) {
-//     this.heap.push(value);
-//     this.siftUp(this.heap.length - 1, this.heap);
-//   }
-// }
+  insert(value) {
+    this.heap.push(value);
+    this.siftUp(this.heap.length - 1, this.heap);
+  }
+}
 
-// function swap(i, j, arr) {
-//   let ele = arr[i];
-//   arr[i] = arr[j];
-//   arr[j] = ele;
-// }
+function swap(i, j, arr) {
+  let ele = arr[i];
+  arr[i] = arr[j];
+  arr[j] = ele;
+}
 
-// let minHeap = new MinHeap([]);
+let minHeap = new MinHeap([]);
 
-// minHeap.insert(3);
-// minHeap.insert(1);
-// minHeap.insert(2);
-// // [1,3,2]
-// console.log(minHeap);
-// // 1
-// console.log(minHeap.peek());
-// // 1
-// console.log(minHeap.remove());
-// //2
-// console.log(minHeap.remove());
-// // 3
-// console.log(minHeap.remove());
-// minHeap.insert(4);
-// minHeap.insert(5);
-// // [4,5]
-// console.log(minHeap);
+minHeap.insert(3);
+minHeap.insert(1);
+minHeap.insert(2);
+// [1,3,2]
+console.log(minHeap);
+// 1
+console.log(minHeap.peek());
+// 1
+console.log(minHeap.remove());
+//2
+console.log(minHeap.remove());
+// 3
+console.log(minHeap.remove());
+minHeap.insert(4);
+minHeap.insert(5);
+// [4,5]
+console.log(minHeap);
 
 //Another way of construct MINHEAP;
 
