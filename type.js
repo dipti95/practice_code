7b47a9d4e57c9cbf2a1776fda5712f75881c1b5a
dipti <dipti.yadav@outlook.com>
2022-01-06 18:52:30 -0500

    type

--- type.js
+++ type.js
@@ -1,5 +1,8 @@
 //return type of value without using typeof method
 
 function type(value) {
+  //Object.prototype.toString.call(value)
+  //return the [Object typeOfValue]
+  // that's why slice 8 , -1
   return Object.prototype.toString.call(value).slice(8, -1);
 }
