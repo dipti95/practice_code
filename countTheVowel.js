c56e6ad525825ab266849b1fae2a842dd486c641
dipti <dipti.yadav@outlook.com>
2022-01-08 16:36:07 -0500

    countTheVowel

--- countTheVowel.js
+++ countTheVowel.js
@@ -3,5 +3,7 @@
 function countTheVowel(str) {
   let vowel = "aeiouAEIOU";
   if (str.length === 0) return 0;
+
+  //vowel.includes(str[0]) => if true then ==1
   return vowel.includes(str[0]) + countTheVowel(str.slice(1));
 }
