d8861ba8aaa0176a11596c96b0b9299d2c5f6695
dipti <dipti.yadav@outlook.com>
2022-04-27 16:11:19 -0400

    solution:Hash Table construction

--- HashTable/construction.js
+++ HashTable/construction.js
@@ -1,6 +1,7 @@
 class HashTable {
   constructor(size) {
     this.data = new Array(size)
+    // this.data = [];
   }
 
   _hash(key) {
@@ -10,10 +11,32 @@ class HashTable {
     }
     return hash
   }
+
+  set(key, value) {
+    let address = this._hash(key)
+    if (!this.data[address]) {
+      this.data[address] = []
+    }
+    this.data[address].push([key, value])
+    return this.data
+  }
+
+  get(key) {
+    const address = this._hash(key)
+    const currentBucket = this.data[address]
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1]
+        }
+      }
+    }
+    return undefined
+  }
 }
 
-//   const myHashTable = new HashTable(50);
-//   myHashTable.set('grapes', 10000)
-//   myHashTable.get('grapes')
-//   myHashTable.set('apples', 9)
-//   myHashTable.get('apples')
+const myHashTable = new HashTable(50)
+myHashTable.set("grapes", 10000)
+myHashTable.get("grapes")
+myHashTable.set("apples", 9)
+myHashTable.get("apples")
