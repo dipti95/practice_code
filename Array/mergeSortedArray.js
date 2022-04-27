663e51ec19b0950db05ab0ae35e56134d63f3bf0
dipti <dipti.yadav@outlook.com>
2022-04-27 15:30:20 -0400

    solution:mergeSortedArray

---
+++ Array/mergeSortedArray.js
@@ -0,0 +1,25 @@
+function mergeSortedArrays(arr1, arr2) {
+  if (!arr1.length) return arr2
+  if (!arr2.length) return arr1
+
+  const mergeArr = []
+  let i = 1
+  let j = 1
+  let ele1 = arr1[0]
+  let ele2 = arr2[0]
+
+  while (ele1 || ele2) {
+    if (ele2 === undefined || ele1 <= ele2) {
+      mergeArr.push(ele1)
+      ele1 = arr1[i]
+      i++
+    } else {
+      mergeArr.push(ele2)
+      ele2 = arr2[j]
+      j++
+    }
+  }
+  return mergeArr
+}
+
+//console.log(mergeSortedArrays([0, 3, 4, 31], [3, 4, 6, 30]))
