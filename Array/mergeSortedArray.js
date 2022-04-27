aba6b5e878ce53144b5cc7e4f64f2eee5a60bcf0
dipti <dipti.yadav@outlook.com>
2022-04-27 15:37:22 -0400

    One more solution: mergeSortedArray

--- Array/mergeSortedArray.js
+++ Array/mergeSortedArray.js
@@ -22,4 +22,10 @@ function mergeSortedArrays(arr1, arr2) {
   return mergeArr
 }
 
+// another solution
+
+function mergeSortedArrays(arr1, arr2) {
+  return [...arr1, ...arr2].sort((a, b) => a - b)
+}
+
 //console.log(mergeSortedArrays([0, 3, 4, 31], [3, 4, 6, 30]))
