3775e1afb87cd08129b253a1aa6866a096adb865
dipti <dipti.yadav@outlook.com>
2022-04-28 15:24:52 -0400

    solution: firstRecurringCharacter

---
+++ Array/firstRecurringCharacter.js
@@ -0,0 +1,23 @@
+//Google Question
+//Given an array = [2,5,1,2,3,5,1,2,4]:
+//It should return 2
+
+//Given an array = [2,1,1,2,3,5,1,2,4]:
+//It should return 1
+
+//Given an array = [2,3,4,5]:
+//It should return undefined
+
+function firstRecurringCharacter(input) {
+  let hash = {}
+  for (const ele of input) {
+    if (ele in hash) return ele
+    else hash[ele] = true
+  }
+  return undefined
+}
+
+// console.log(firstRecurringCharacter([2, 5, 1, 2, 3, 5, 1, 2, 4]))
+// console.log(firstRecurringCharacter([2, 1, 1, 2, 3, 5, 1, 2, 4]))
+// console.log(firstRecurringCharacter([2, 3, 4, 5]))
+// console.log(firstRecurringCharacter([2, 5, 5, 2, 3, 5, 1, 2, 4]))
