7f84471f384ef0baf6eea8e361df5cd5fbf365c6
dipti <dipti.yadav@outlook.com>
2022-05-18 05:30:55 -0400

    solution: find loop -LinkedList

--- AlgoExpertProblems/LinkedList/findLoop.js
+++ AlgoExpertProblems/LinkedList/findLoop.js
@@ -5,6 +5,12 @@ class LinkedList {
   }
 }
 
+/* 
+Think about rabbit and turtle race.If rabbit and turtle is running in a circular path
+and speed of rabbit is faster than turtle which is obvious.
+So if rabbit find turtle 
+while running it means they are running in a circular path
+*/
 function findLoop(head) {
   let nodeOne = head.next
   let nodeTwo = head.next.next
