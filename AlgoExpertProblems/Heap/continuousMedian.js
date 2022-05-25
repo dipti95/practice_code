a95864ac5514d696eb773808276488a2716939a6
dipti <dipti.yadav@outlook.com>
2022-05-25 17:28:26 -0400

    modified: continuousMedian

--- AlgoExpertProblems/Heap/continuousMedian.js
+++ AlgoExpertProblems/Heap/continuousMedian.js
@@ -1,36 +1,36 @@
 class ContinuousMedianHandler {
   constructor() {
-    this.lower = new ConstructHeap(MaxHeap, []);
-    this.higher = new ConstructHeap(MinHeap, []);
-    this.median = null;
+    this.lower = new ConstructHeap(MaxHeap, [])
+    this.higher = new ConstructHeap(MinHeap, [])
+    this.median = null
   }
 
   insert(value) {
     if (this.lower.length <= 0 || value < this.lower.peek()) {
-      this.lower.insert(number);
-    } else this.higher.insert(number);
-    this.rebalanceHeap();
-    this.updateMedian();
+      this.lower.insert(number)
+    } else this.higher.insert(number)
+    this.rebalanceHeap()
+    this.updateMedian()
   }
 
   rebalanceHeap() {
     if (this.lower.length - this.higher.length === 2) {
-      this.higher.insert(this.lower.remove());
+      this.higher.insert(this.lower.remove())
     } else if (this.higher.length - this.lower.length === 2) {
-      this.lower.insert(this.higher.remove());
+      this.lower.insert(this.higher.remove())
     }
   }
 
   updateMedian() {
     if (this.higher.length === this.lower.length) {
-      this.median = (this.lower.peek() + this.higher.peek()) / 2;
+      this.median = (this.lower.peek() + this.higher.peek()) / 2
     } else if (this.lower.length > this.higher.length)
-      this.median = this.lower.peek();
-    else this.median = this.higher.peek();
+      this.median = this.lower.peek()
+    else this.median = this.higher.peek()
   }
 
   getMedian() {
-    return this.median;
+    return this.median
   }
 }
 
@@ -39,88 +39,90 @@ class ConstructHeap {
   constructor(heapType, array) {
     // heapType is function by which we would find the type of heap min & max
 
-    this.heapType = heapType;
-    this.heap = this.buildHeap(array);
-    this.length = this.heap.length;
+    this.heapType = heapType
+    this.heap = this.buildHeap(array)
+    this.length = this.heap.length
   }
 
   buildHeap(array) {
     // Write your code here.
-    let parentNodeIdx = Math.floor((array.length - 2) / 2);
+    let parentNodeIdx = Math.floor((array.length - 2) / 2)
     for (let i = parentNodeIdx; i >= 0; i--) {
-      this.siftDown(i, array.length - 1, array);
+      this.siftDown(i, array.length - 1, array)
     }
-    return array;
+    return array
   }
 
   siftDown(currentIdx, endIdx, heap) {
     // Write your code here.
-    let firstChildIdx = currentIdx * 2 + 1;
+    let firstChildIdx = currentIdx * 2 + 1
     while (firstChildIdx <= endIdx) {
       let secondChildIdx =
-        currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1;
-      let swapIdx = 0;
+        currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1
+      let swapIdx = 0
       if (secondChildIdx !== -1) {
         if (this.heapType(heap[secondChildIdx], heap[firstChildIdx])) {
-          swapIdx = secondChildIdx;
+          swapIdx = secondChildIdx
         } else {
-          swapIdx = firstChildIdx;
+          swapIdx = firstChildIdx
         }
       } else {
-        swapIdx = firstChildIdx;
+        swapIdx = firstChildIdx
       }
       if (this.heapType(heap[swapIdx], heap[currentIdx])) {
-        swap(currentIdx, swapIdx, heap);
-        currentIdx = swapIdx;
-        firstChildIdx = currentIdx * 2 + 1;
+        swap(currentIdx, swapIdx, heap)
+        currentIdx = swapIdx
+        firstChildIdx = currentIdx * 2 + 1
       } else {
-        return;
+        return
       }
     }
   }
 
   siftUp(currentIdx, heap) {
-    let parentIdx = Math.floor((currentIdx - 1) / 2);
+    let parentIdx = Math.floor((currentIdx - 1) / 2)
     while (currentIdx > 0) {
       if (this.heapType(heap[currentIdx], heap[parentIdx])) {
-        swap(currentIdx, parentIdx, heap);
-        currentIdx = parentIdx;
-        parentIdx = Math.floor((currentIdx - 1) / 2);
-      } else return;
+        swap(currentIdx, parentIdx, heap)
+        currentIdx = parentIdx
+        parentIdx = Math.floor((currentIdx - 1) / 2)
+      } else return
     }
   }
 
   peek() {
-    return this.heap[0];
+    return this.heap[0]
   }
 
   remove() {
-    swap(0, this.length - 1, this.heap);
-    let removeVal = this.heap.pop();
-    this.length--;
-    this.siftDown(0, this.length - 1, this.heap);
-    return removeVal;
+    swap(0, this.length - 1, this.heap)
+    let removeVal = this.heap.pop()
+    this.length--
+    this.siftDown(0, this.length - 1, this.heap)
+    return removeVal
   }
 
   insert(value) {
-    this.heap.push(value);
-    this.length++;
-    this.siftUp(this.length - 1, this.heap);
+    this.heap.push(value)
+    this.length++
+    this.siftUp(this.length - 1, this.heap)
   }
 }
 
 function swap(i, j, arr) {
-  let ele = arr[i];
-  arr[i] = arr[j];
-  arr[j] = ele;
+  let ele = arr[i]
+  arr[i] = arr[j]
+  arr[j] = ele
 }
 
+// Min heap and Max heap are the camparison function,
+// used in siftDown and siftUp
 // higher half  => min heap
 function MinHeap(a, b) {
-  return a < b;
+  return a < b
 }
 
 // Lower half => max heap
 function MaxHeap(a, b) {
-  return a > b;
+  return a > b
 }
