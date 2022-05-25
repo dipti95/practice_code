1826c56c1c779f1e298758871bd0eb17b53da672
dipti <dipti.yadav@outlook.com>
2022-05-25 18:18:24 -0400

    sortKSortedArray

--- AlgoExpertProblems/Heap/sortKSortedArray.js
+++ AlgoExpertProblems/Heap/sortKSortedArray.js
@@ -1,6 +1,7 @@
 function sortKSortedArray(array, k) {
-  // Hear we are doing Math.min because if k+1 would be beyond the array.length than we want array.length . otherwise after k+1 the value will be undefined;
-  let heap = new MinHeap(array.slice(0, Math.min(k + 1, array.length)));
+  // Hear we are doing Math.min because if k+1 would be beyond the array.length than we want array.length.
+  //otherwise after k+1 the value will be undefined;
+  let heap = new MinHeap(array.slice(0, Math.min(k + 1, array.length)))
   /* For Example we have array = [3,2,1,5,4,7,6,5]  k =3;
      
      [3 , 2 , 1 , 5 , 4 , 7 , 6 , 5]
@@ -15,101 +16,102 @@ So, hear in example heap is from 0 to k+1(4)
   IN MIN HEAP  CONSTRUCTION  REMOVE METHOD REMOVES THE ROOT VALUE OF HEAP;
 
  */
-  let nextIdx = 0;
+  let nextIdx = 0
 
   /*  With the help of above comments now it is understandable that why  we are taking i = k+1
-  because after removing the value and set that remove value  to the nextIdx (which i define above In the start nextIdx is always zero)of array 
+  because after removing the value and set that remove value  to the nextIdx 
+  (which i define above In the start nextIdx is always zero)of array 
   we have to insert the next value and hear it is k+1 upto array.length-1 
    */
 
   for (let i = k + 1; i < array.length; i++) {
-    let minElement = heap.remove();
-    array[nextIdx] = minElement;
-    nextIdx += 1;
+    let minElement = heap.remove()
+    array[nextIdx] = minElement
+    nextIdx += 1
 
-    let currentElement = array[i];
+    let currentElement = array[i]
     /**  In the first iteration i= k+1  so if  we look to the given array => array[k+1] is 4
        SO, hear we are inserting the values from  k+1 index to the length of the array 
       */
-    heap.insert(currentElement);
+    heap.insert(currentElement)
   }
 
   /* In case if heap is not empty than we are doing same process in this while loop*/
 
   while (!heap.isEmpty()) {
-    let minElement = heap.remove();
-    array[nextIdx] = minElement;
-    nextIdx += 1;
+    let minElement = heap.remove()
+    array[nextIdx] = minElement
+    nextIdx += 1
   }
-  return array;
+  return array
 }
 
 // Heap construction ;
 class MinHeap {
   constructor(array) {
-    this.heap = this.buildHeap(array);
+    this.heap = this.buildHeap(array)
   }
 
   buildHeap(array) {
-    let parentNodeIdx = Math.floor((array.length - 2) / 2);
+    let parentNodeIdx = Math.floor((array.length - 2) / 2)
     for (let i = parentNodeIdx; i >= 0; i--) {
-      this.siftDown(i, array.length - 1, array);
+      this.siftDown(i, array.length - 1, array)
     }
-    return array;
+    return array
   }
 
   siftDown(currentIdx, endIdx, heap) {
-    let firstChildIdx = currentIdx * 2 + 1;
+    let firstChildIdx = currentIdx * 2 + 1
     while (firstChildIdx <= endIdx) {
       let secondChildIdx =
-        currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1;
-      let swapIdx = 0;
+        currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1
+      let swapIdx = 0
       if (secondChildIdx !== -1 && heap[secondChildIdx] < heap[firstChildIdx]) {
-        swapIdx = secondChildIdx;
+        swapIdx = secondChildIdx
       } else {
-        swapIdx = firstChildIdx;
+        swapIdx = firstChildIdx
       }
       if (heap[swapIdx] < heap[currentIdx]) {
-        swap(currentIdx, swapIdx, heap);
-        currentIdx = swapIdx;
-        firstChildIdx = currentIdx * 2 + 1;
+        swap(currentIdx, swapIdx, heap)
+        currentIdx = swapIdx
+        firstChildIdx = currentIdx * 2 + 1
       } else {
-        return;
+        return
       }
     }
   }
 
   siftUp(currentIdx, heap) {
-    let parentIdx = Math.floor((currentIdx - 1) / 2);
+    let parentIdx = Math.floor((currentIdx - 1) / 2)
     while (currentIdx > 0 && heap[currentIdx] < heap[parentIdx]) {
-      swap(currentIdx, parentIdx, heap);
-      currentIdx = parentIdx;
-      parentIdx = Math.floor((currentIdx - 1) / 2);
+      swap(currentIdx, parentIdx, heap)
+      currentIdx = parentIdx
+      parentIdx = Math.floor((currentIdx - 1) / 2)
     }
   }
 
   peek() {
-    return this.heap[0];
+    return this.heap[0]
   }
   isEmpty() {
-    return this.heap.length === 0;
+    return this.heap.length === 0
   }
 
   remove() {
-    swap(0, this.heap.length - 1, this.heap);
-    let removeVal = this.heap.pop();
-    this.siftDown(0, this.heap.length - 1, this.heap);
-    return removeVal;
+    swap(0, this.heap.length - 1, this.heap)
+    let removeVal = this.heap.pop()
+    this.siftDown(0, this.heap.length - 1, this.heap)
+    return removeVal
   }
 
   insert(value) {
-    this.heap.push(value);
-    this.siftUp(this.heap.length - 1, this.heap);
+    this.heap.push(value)
+    this.siftUp(this.heap.length - 1, this.heap)
   }
 }
 
 function swap(i, j, arr) {
-  let ele = arr[i];
-  arr[i] = arr[j];
-  arr[j] = ele;
+  let ele = arr[i]
+  arr[i] = arr[j]
+  arr[j] = ele
 }
