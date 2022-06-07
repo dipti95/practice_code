2968293a6b5233f8c05e2bca20086d4f8c20fc7e
dipti <dipti.yadav@outlook.com>
2022-06-07 14:24:01 -0400

    reverseWordsInString solution without using split

--- AlgoExpertProblems/TYPESCRIPT_SOLUTIONS/String/reverseWordsInString.ts
+++ AlgoExpertProblems/TYPESCRIPT_SOLUTIONS/String/reverseWordsInString.ts
@@ -6,3 +6,23 @@ function reverseWordsInString(string: string) {
   }
   return resultArr.join(" ")
 }
+
+// without using split or reverse
+
+function reverseWordsInString(string: string) {
+  let arr: Array<string> = []
+  let char = ""
+  let resultArr: Array<string> = []
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === " ") {
+      arr.push(char)
+      char = ""
+    } else char += string[i]
+  }
+  arr.push(char)
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    resultArr.push(arr[i])
+  }
+  return resultArr.join(" ")
+}
