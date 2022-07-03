3cf5e0c4824aaddbc143e2c44749787d1b43bf3c
dipti <dipti.yadav@outlook.com>
2022-07-03 19:27:43 -0400

    searchInSortedMatrix : Better solution

--- AlgoExpertProblems/TYPESCRIPT_SOLUTIONS/Searching/searchInSortedMatrix.ts
+++ AlgoExpertProblems/TYPESCRIPT_SOLUTIONS/Searching/searchInSortedMatrix.ts
@@ -8,3 +8,18 @@ function searchInSortedMatrix(matrix: number[][], target: number) {
   }
   return [-1, -1]
 }
+
+// Its given that row and col are sorted
+
+function searchInSortedMatrix(matrix: number[][], target: number) {
+  let row = 0
+  let col = matrix[0].length - 1
+
+  while (col >= 0 && row < matrix.length) {
+    if (target < matrix[row][col]) col--
+    else if (target > matrix[row][col]) row++
+    else return [row, col]
+  }
+
+  return [-1, -1]
+}
