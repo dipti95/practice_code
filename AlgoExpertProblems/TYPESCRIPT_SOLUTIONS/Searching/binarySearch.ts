56fa8ad217c21ee2a9329adbe4e41ff72426a9c8
dipti <dipti.yadav@outlook.com>
2022-07-07 14:17:36 -0400

    binary search solution

--- AlgoExpertProblems/TYPESCRIPT_SOLUTIONS/Searching/binarySearch.ts
+++ AlgoExpertProblems/TYPESCRIPT_SOLUTIONS/Searching/binarySearch.ts
@@ -9,3 +9,23 @@ function binarySearch(array: number[], target: number): number {
     return binarySearch(array.slice(0, midIdx), target)
   else return midIdx
 }
+
+//  Better solution
+function search(nums: number[], target: number): number {
+  let left: number = 0
+  let right: number = nums.length - 1
+
+  while (left <= right) {
+    let middleIdx: number = Math.floor((left + right) / 2)
+
+    if (nums[middleIdx] > target) {
+      right = middleIdx - 1
+    } else if (nums[middleIdx] < target) {
+      left = middleIdx + 1
+    } else {
+      return middleIdx
+    }
+  }
+
+  return -1
+}
