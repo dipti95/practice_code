8ed9ecaea9daf6a8013085a2267d9d334921c3fe
dipti <dipti.yadav@outlook.com>
2022-07-19 16:02:07 -0400

    longestSubstringWithoutDuplication solution O(n)time

--- AlgoExpertProblems/TYPESCRIPT_SOLUTIONS/String/longestSubstringWithoutDuplicate.ts
+++ AlgoExpertProblems/TYPESCRIPT_SOLUTIONS/String/longestSubstringWithoutDuplicate.ts
@@ -1,4 +1,4 @@
-//0(n*n) time
+//O(n*n) time
 
 function longestSubstringWithoutDuplication(string: string) {
   let substring: string = ""
@@ -12,3 +12,25 @@ function longestSubstringWithoutDuplication(string: string) {
   }
   return substring
 }
+
+//O(n) time
+
+longestSubstringWithoutDuplication(string: string) {
+  
+    let hash :{[key:string]:number}={}
+    let start =0
+    let longestSubstring =[0,1]
+  
+    for(let i =0;i< string.length;i++){
+      if(string[i] in hash){
+        start = Math.max(start, hash[string[i]]+1)
+      }
+      if(longestSubstring[1]-longestSubstring[0] < i+1 -start){
+        longestSubstring=[start, i+1]
+      }
+  
+      hash[string[i]]=i
+    }
+   return string.slice(longestSubstring[0], longestSubstring[1])
+  }
+  
