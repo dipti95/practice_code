028cee7672d5f7422f827eae9d86e12936c6061e
dipti <dipti.yadav@outlook.com>
2022-07-19 15:11:11 -0400

    longestSubstringWithoutDuplication solution O(n2)

---
+++ AlgoExpertProblems/TYPESCRIPT_SOLUTIONS/String/longestSubstringWithoutDuplicate.ts
@@ -0,0 +1,16 @@
+//0(n*n) time
+
+function longestSubstringWithoutDuplication(string: string) {
+  let substring: string = ""
+  for (let i = 0; i < string.length; i++) {
+    let current: string = string[i]
+    for (let j = i + 1; j < string.length; j++) {
+      if (current.includes(string[j])) break
+      current += string[j]
+    }
+    if (substring.length < current.length) substring = current
+  }
+  console.log(substring)
+
+  return substring
+}
