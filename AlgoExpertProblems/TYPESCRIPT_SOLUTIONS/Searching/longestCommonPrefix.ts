db1f4a19e627d3baaaf5a8ac58b1689fbb24bd20
dipti <dipti.yadav@outlook.com>
2022-08-02 14:22:28 -0400

    longestCommonPrefix solution using binary search

---
+++ AlgoExpertProblems/TYPESCRIPT_SOLUTIONS/Searching/longestCommonPrefix.ts
@@ -0,0 +1,33 @@
+function longestCommonPrefix(strs: string[]): string {
+  if (strs == null || strs.length == 0) return ""
+  let l = 1
+  let r = Infinity
+
+  for (const ele of strs) {
+    r = Math.min(r, ele.length)
+  }
+
+  while (l <= r) {
+    let mid = Math.floor((l + r) / 2)
+
+    if (checkIfPrefix(strs, mid)) {
+      l = mid + 1
+    } else {
+      r = mid - 1
+    }
+  }
+
+  return strs[0].substr(0, (l + r) / 2)
+}
+
+function checkIfPrefix(str: string[], length: number) {
+  const res = str[0].substr(0, length)
+
+  for (let i = 1; i < str.length; i++) {
+    if (!str[i].startsWith(res)) {
+      return false
+    }
+  }
+
+  return true
+}
