b96541fa331eb74b0c1059f9572d4e3c630dccf7
dipti <dipti.yadav@outlook.com>
2022-09-25 17:17:40 -0400

    reorderLinkedList

--- leetCodeProblem/LinkedList/143-ReorderList.ts
+++ leetCodeProblem/LinkedList/143-ReorderList.ts
@@ -1,3 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
 function reorderList(head: ListNode | null): void {
   let slow: ListNode | null = head
   let fast: ListNode | null = head.next
