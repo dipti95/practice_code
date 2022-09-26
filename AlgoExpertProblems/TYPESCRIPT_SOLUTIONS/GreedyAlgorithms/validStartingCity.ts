15cf4848ae1104309633b8995eb88adf892a0bf5
dipti <dipti.yadav@outlook.com>
2022-09-26 14:57:20 -0400

    validStartingCity in O(n) time

--- AlgoExpertProblems/TYPESCRIPT_SOLUTIONS/GreedyAlgorithms/validStartingCity.ts
+++ AlgoExpertProblems/TYPESCRIPT_SOLUTIONS/GreedyAlgorithms/validStartingCity.ts
@@ -16,3 +16,23 @@ function validStartingCity(distances: number[], fuel: number[], mpg: number) {
 
   return -1
 }
+
+function validStartingCity(distances: number[], fuel: number[], mpg: number) {
+  //O(n)
+
+  let milesLeft = 0
+  let minGas = 0
+  let minCity = 0
+  // starting from 1 because we aready set the value of milesLeft,minGas and minCity.
+  for (let city = 1; city < distances.length; city++) {
+    //fuel[city-1] => fuel of prev city
+    //distances[city-1] => distance covered from prev city
+    milesLeft += mpg * fuel[city - 1] - distances[city - 1]
+    if (milesLeft < minGas) {
+      minGas = milesLeft
+      minCity = city
+    }
+  }
+
+  return minCity
+}
