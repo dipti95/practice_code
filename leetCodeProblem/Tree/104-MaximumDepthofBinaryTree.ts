788ebb89d59bef03d1c6d1b68ef3430facdea9e6
dipti <dipti.yadav@outlook.com>
2022-10-11 17:47:42 -0400

    maxDepth of tree

--- leetCodeProblem/Tree/104-MaximumDepthofBinaryTree.ts
+++ leetCodeProblem/Tree/104-MaximumDepthofBinaryTree.ts
@@ -3,3 +3,20 @@ function maxDepth(root: TreeNode | null): number {
 
   return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
 }
+
+// another solution=> easy to understand
+
+function maxDepth(root: TreeNode | null): number {
+  if (root === null) return 0
+
+  return dfs(root)
+}
+
+function dfs(root) {
+  let leftHeight = maxDepth(root.left)
+  let rightHeight = maxDepth(root.right)
+
+  let maxHeight = Math.max(leftHeight, rightHeight)
+
+  return maxHeight + 1
+}
