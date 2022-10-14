9f616ab8105014cc757bb27d0e8b285ac3d1a6d3
dipti <dipti.yadav@outlook.com>
2022-10-14 14:55:30 -0400

    DiameterOfBinaryTree using dfs

---
+++ leetCodeProblem/Tree/543-DiameterofBinaryTree.ts
@@ -0,0 +1,29 @@
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val
+    this.left = left === undefined ? null : left
+    this.right = right === undefined ? null : right
+  }
+}
+
+function diameterOfBinaryTree(root: TreeNode | null): number {
+  let res: number = 0
+
+  function dfs(root: TreeNode | null) {
+    if (root === null) return -1
+
+    let left = dfs(root.left)
+    let right = dfs(root.right)
+
+    res = Math.max(res, 2 + left + right)
+
+    return 1 + Math.max(left, right)
+  }
+
+  dfs(root)
+
+  return res
+}
