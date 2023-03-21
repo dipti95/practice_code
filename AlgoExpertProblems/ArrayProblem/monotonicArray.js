d08567a1bd5a9cdb796a7667970da743c4a5d722
dipti <dipti.yadav@outlook.com>
2023-03-21 17:04:05 -0400

    monotonic array problem

---
+++ AlgoExpertProblems/ArrayProblem/monotonicArray.js
@@ -0,0 +1,15 @@
+function isMonotonic(array) {
+  let increasingOrder = false
+  let decresingOrder = false
+
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] === array[i + 1]) continue
+    if (array[i] > array[i + 1]) {
+      decresingOrder = true
+    }
+    if (array[i] < array[i + 1]) increasingOrder = true
+  }
+
+  if (increasingOrder && decresingOrder) return false
+  else return true
+}
